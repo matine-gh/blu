b65cd3052a22a7c3cb68104d754376a568ee5648
matine <mghazanfari@tarasystem.ch>
2024-07-30 15:40:46 +0330

    [feat]-add Icons component

---
+++ public/Icons/index.tsx
@@ -0,0 +1,14 @@
+import {IconsProps} from "./Icons.interface";
+
+export default function Icons(props: IconsProps) {
+    switch (props.name) {
+        case 'google':
+            return (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            )
+        default:
+            return ('')
+    }
+}
