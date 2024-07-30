aba9433837c26d9a4f6c8c10024d3af7223659f9
matine <mghazanfari@tarasystem.ch>
2024-07-30 15:53:23 +0330

    [fix]-add dynamic width and height for icons

--- public/Icons/index.tsx
+++ public/Icons/index.tsx
@@ -4,7 +4,7 @@ export default function Icons(props: IconsProps) {
     switch (props.name) {
         case 'more':
             return (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" width={props.width || '24'} height={props.height || '24'} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
                 </svg>
             )
