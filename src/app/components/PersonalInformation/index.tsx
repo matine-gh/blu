20d575a8fc14cf634ea57e6c6d79269f943e2817
matine <mghazanfari@tarasystem.ch>
2024-07-30 18:42:34 +0330

    [feat]-init PersonalInformation

---
+++ src/app/components/PersonalInformation/index.tsx
@@ -0,0 +1,10 @@
+import PersonalForm from "@/app/components/PersonalInformation/PersonalForm";
+
+export default function PersonalInformation() {
+    return (
+        <div>
+            <h1>فرم اطلاعات متقاضی</h1>
+            <PersonalForm />
+        </div>
+    )
+}
