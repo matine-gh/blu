e316811166749aa104ec77180b7fd55f0db65a55
matine <mghazanfari@tarasystem.ch>
2024-07-31 07:21:01 +0330

    [feat]-personal information UI done

--- src/app/components/PersonalInformation/PersonalForm/index.tsx
+++ src/app/components/PersonalInformation/PersonalForm/index.tsx
@@ -1,5 +1,79 @@
+'use client'
+import {useDispatch} from "react-redux";
+import {postLoansLoading} from "@/store/postLoans/action";
+
+import { useFormik } from 'formik';
+
 export default function PersonalForm() {
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            nid: '',
+            birthday: '',
+            phoneNumber: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
-        <div>form</div>
+        <form onSubmit={formik.handleSubmit} className={'md:grid grid-cols-5 grid-rows-4 gap-y-8 gap-x-4 [&>*]:max-w-72'}>
+            <div className="col-span-2">
+                <label htmlFor="firstName">نام</label>
+                <input
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.firstName}
+                />
+            </div>
+            <div className="col-span-2 col-start-4">
+                <label htmlFor="lastName">نام خانوادگی</label>
+                <input
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    required={true}
+                    onChange={formik.handleChange}
+                    value={formik.values.lastName}
+                />
+            </div>
+            <div className="col-span-2 row-start-2">
+                <label htmlFor="nid">کد ملی</label>
+                <input
+                    id="nid"
+                    name="nid"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.nid}
+                />
+            </div>
+            <div className="group col-span-2 col-start-4 row-start-2">
+                <label clhtmlFor="col-span-2 row-start-1 group-focus:text-red-600">تاریخ تولد</label>
+                <input
+                    id="birthday"
+                    name="birthday"
+                    type="text"
+                    className={''}
+                    onChange={formik.handleChange}
+                    value={formik.values.birthday}
+                />
+            </div>
+            <div className={'col-span-2 row-start-3'}>
+                <label htmlFor="phoneNumber">شماره تماس</label>
+                <input
+                    id="phoneNumber"
+                    name="phoneNumber"
+                    type="text"
+                    onChange={formik.handleChange}
+                    value={formik.values.phoneNumber}
+                />
+            </div>
+            <button className={'primary-button'+' '+'col-start-3 row-start-4 max-md:my-4 max-h-12'} type="submit">ثبت اطلاعات</button>
+        </form>
     )
 }
--- src/app/components/PersonalInformation/index.tsx
+++ src/app/components/PersonalInformation/index.tsx
@@ -2,7 +2,7 @@ import PersonalForm from "@/app/components/PersonalInformation/PersonalForm";
 
 export default function PersonalInformation() {
     return (
-        <div>
+        <div className={'container'}>
             <h1>فرم اطلاعات متقاضی</h1>
             <PersonalForm />
         </div>
