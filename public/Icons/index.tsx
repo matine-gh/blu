3218206fa508f23b802b74086994b3d5c099cdb4
matine <mghazanfari@tarasystem.ch>
2024-08-01 08:28:07 +0330

    [fix]- change back icon

--- public/Icons/index.tsx
+++ public/Icons/index.tsx
@@ -16,8 +16,8 @@ export default function Icons(props: IconsProps) {
             )
         case 'back':
             return (
-                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} width={props.width || '24'} height={props.height || '24'} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                 </svg>
             )
         default:
